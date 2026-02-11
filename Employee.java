dc90a37f4b1a7dae652c2ec6c8df484c6f1ed5b1
Pankaj Vishnu Dagade <pankajdagade84@gmail.com>
2026-02-11 08:04:18 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,62 @@
+public class Employee {
+
+    private int id;
+    private String name;
+    private String gender;
+    private String department;
+    private double salary;
+
+    // No-arg constructor
+    public Employee() {
+    }
+
+    // All-args constructor
+    public Employee(int id, String name, String gender, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
